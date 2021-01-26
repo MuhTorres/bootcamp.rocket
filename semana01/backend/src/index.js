7b3e3a9cc03cb877f77c64be5b98eec2ad3700a2
Murilo Torres <muhtorres@hotmail.com>
2021-01-26 15:38:00 -0300

    Adiciona projeto semana01 ao repo

---
+++ semana01/backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/projects", (req, res) => { 
+    return res.json(["projeto 1", "projeto 2"]);
+});
+
+app.post("/projects", (req, res) => {
+    return res.json(["projeto 1", "projeto 2", "projeto 3"]);
+});
+
+
+app.put("/projects/:id", (req, res) => {
+    return res.json(["projeto 4", "projeto 2", "projeto 3"]);
+});
+
+app.delete("/projects/:id", (req, res) => {
+    return res.json(["projeto 2", "projeto 3"]);
+});
+
+app.listen(3333, () => {
+    console.log("Backend started!");
+});
